09b0167c77d522d410e3afc513f8e054b8609192
unknown <eithandarmon9911@gmail.com>
2024-03-18 20:56:58 +0630

    ui update

--- src/components/Home/Footer.jsx
+++ src/components/Home/Footer.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
-import home from '../../assets/img/footerIcons/home.png';
-import promo from '../../assets/img/footerIcons/promo.png';
-import livechat from '../../assets/img/footerIcons/livechat.png';
-import wallet from '../../assets/img/footerIcons/wallet.png';
-import history from '../../assets/img/history.png';
-import summary from '../../assets/img/summary.png';
+// import home from '../../assets/img/footerIcons/home.png';
+// import promo from '../../assets/img/footerIcons/promo.png';
+// import livechat from '../../assets/img/footerIcons/livechat.png';
+// import wallet from '../../assets/img/footerIcons/wallet.png';
+// import history from '../../assets/img/history.png';
+// import summary from '../../assets/img/summary.png';
+
+import '../../assets/css/footer.css'
+
+import { IoHome } from "react-icons/io5";
+import { IoGiftSharp } from "react-icons/io5";
+import { LuFileClock } from "react-icons/lu";
+import { IoNewspaper } from "react-icons/io5";
 
 import profile from '../../assets/img/footerIcons/account.png';
 import { NavLink } from 'react-router-dom';
 
 const Footer = () => {
   const footerIcons = [
-    { img: home, title: 'Home', link: '/' },
-    { img: promo, title: 'Promotion', link: '/promotion' },
-    { img: history, title: 'History', link: '/history' },
-    { img: summary, title: 'Game Log', link: '/game-log' },
+    { img: IoHome, title: 'Home', link: '/' },
+    { img: IoGiftSharp, title: 'Promotion', link: '/promotion' },
+    { img: LuFileClock, title: 'History', link: '/history' },
+    { img: IoNewspaper, title: 'GameLog', link: '/game-log' },
   ];
   return (
     <footer className=' rounded-top-5 px-3 px-sm-5 py-3 fixed-bottom   z-3 d-flex  align-items-center justify-content-between mt-5'>
@@ -23,13 +30,10 @@ const Footer = () => {
           <NavLink
             key={index}
             to={item.link}
-            className='text-light d-flex flex-column text-center align-align-items-center text-decoration-none'
+            className='text-light d-flex flex-column text-center align-items-center text-decoration-none'
           >
-            <img
-              src={item.img}
-              className='footerImg mx-auto'
-              alt={item.title}
-            />
+            <item.img size={30} 
+            style={{color:'#80bdff'}}/>
             <span className='footerTitle'>{item.title}</span>
           </NavLink>
         );
--- src/components/Home/Marquee.jsx
+++ src/components/Home/Marquee.jsx
@@ -2,15 +2,22 @@ import React from 'react'
 import useFetch from '../../hooks/useFetch'
 import BASE_URL from '../../hooks/baseURL'
 
+import { HiSpeakerphone } from "react-icons/hi";
+import { FaEnvelopeOpen } from "react-icons/fa";
+
 const Marquee = () => {
   const { data: bannerText } = useFetch(BASE_URL + '/bannerText');
   // console.log(bannerText.text);
   return (
-    <div>
-      <marquee className='p-2 shadow-lg rounded  text-light py-1 px-2 m-2 border border-warning '>
+    <div style={{position:'relative'}}>
+      <HiSpeakerphone size={30} className='p-2 rounded' color='#ddd' style={{position:'absolute',left:'10px',top:'10px',background:'#00354d'}} />
+      <marquee className='p-2 shadow-lg rounded  text-light py-1 px-2 m-2 border border-warning' >
         {bannerText && bannerText.text}
         {!bannerText && 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur nobis tempora cupiditate quaerat pariatur consequuntur libero a, necessitatibus rerum nemo eius voluptatibus! Voluptatum ducimus dolore alias laboriosam, accusamus harum vitae.'}
       </marquee>
+      <div>
+      <FaEnvelopeOpen size={30} color='#ddd' className='p-2 rounded' style={{position:'absolute',right:'10px',top:'10px',background:'#00354d'}}/>
+      </div>
     </div>
   )
 }
